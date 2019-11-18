63e4c42a3070fe4b6373a72070f3aa8836975e09
guilvicent3 <36491077+guilvicent3@users.noreply.github.com>
2019-11-18 16:43:08 +0100

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,19 @@
+<?php 
+$target_dir = "/upload/images";
+$image = $_POST["image"];
+
+if(!file_exists($target_dir)){
+    mkdir($target_dir, 0777, true);
+}
+
+$target_dir= $target_dir ."/" . rand() ."_" . time() . ".jpeg";
+
+if(file_put_contents($target_dir, base64_decode($image))){
+    echo json_encode([
+    "Message" => "The file has been uploaded.".
+    "Status" => "OK"]);
+}else{
+    echo json_encode([
+    "Message" => "There was an error uploading the file",
+    "Status" => "Error" ]);
+}
